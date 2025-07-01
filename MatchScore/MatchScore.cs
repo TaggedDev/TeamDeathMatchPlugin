6ba9b5fc18c872d4afb30fbe29933dc4a2402d62
Mikov Denis <denis.mikov.06@gmail.com>
2025-07-01 16:44:32 +0300

    feat(match): implemented PvP kill handler

--- MatchScore/MatchScore.cs
+++ MatchScore/MatchScore.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using OpenMod.API.Eventing;
 using OpenMod.API.Ioc;
@@ -6,6 +7,7 @@ using OpenMod.Unturned.Players.Life.Events;
 using OpenMod.Unturned.Users;
 using Scitalis.TDM.TeamManagement;
 using SDG.Unturned;
+using Steamworks;
 using UnityEngine;
 using Vector3 = System.Numerics.Vector3;
 
@@ -16,6 +18,8 @@ namespace Scitalis.TDM.MatchScore
     {
         private readonly ITeamDataStore _teamData;
         private readonly IUnturnedUserDirectory _userDirectory;
+        
+        public Dictionary<string, int> TeamScores { get; private set; } = new Dictionary<string, int>();
 
         public MatchScore(ITeamDataStore teamData, IUnturnedUserDirectory userDirectory)
         {
@@ -25,17 +29,19 @@ namespace Scitalis.TDM.MatchScore
 
         public void ResetProgress()
         {
-            throw new System.NotImplementedException();
+            foreach (KeyValuePair<string, int> teamScore in TeamScores) 
+                TeamScores[teamScore.Key] = 0;
         }
 
         public void SetProgress(int score)
         {
-            throw new System.NotImplementedException();
+            foreach (KeyValuePair<string, int> teamScore in TeamScores)
+                TeamScores[teamScore.Key] = score;
         }
 
         public void SetProgress(string teamName, int score)
         {
-            throw new System.NotImplementedException();
+            TeamScores[teamName] = score;
         }
 
         public Task HandleEventAsync(object? sender, UnturnedPlayerDeathEvent @event)
@@ -45,7 +51,11 @@ namespace Scitalis.TDM.MatchScore
             if (victimUser == null)
                 return Task.CompletedTask;
 
-            UnturnedUser? killerUser = _userDirectory.FindUser(@event.Instigator) ?? victimUser;
+            UnturnedUser? killerUser = _userDirectory.FindUser(@event.Instigator);
+            if (killerUser == null)
+                killerUser = victimUser;
+            else
+                HandlePvPKill(killerUser.SteamId, victimUser.SteamId);
 
             Vector3 victimPosition = victimUser.Player.Transform.Position;
             float distance = Vector3.Distance(victimPosition, killerUser.Player.Transform.Position);
@@ -59,6 +69,28 @@ namespace Scitalis.TDM.MatchScore
                        $"{killerName} using {weapon} ({distance}m)";
             ChatManager.serverSendMessage(message, Color.gray);
             return Task.CompletedTask;
+        }
+
+        private void HandlePvPKill(CSteamID killerUserSteamId, CSteamID victimUserSteamId)
+        {
+            string killerTeam = _teamData.GetTeam(killerUserSteamId);
+            string victimTeam = _teamData.GetTeam(victimUserSteamId);
+            if (killerTeam == victimTeam)
+                HandleTeamKill(killerTeam);
+            else
+                HandleEnemyKill(killerTeam);
+        }
+
+        private void HandleEnemyKill(string killerTeam)
+        {
+            if (!TeamScores.TryAdd(killerTeam, -1))
+                TeamScores[killerTeam] -= 1;
+        }
+
+        private void HandleTeamKill(string killerTeam)
+        {
+            if (!TeamScores.TryAdd(killerTeam, 1))
+                TeamScores[killerTeam] += 1;
         }
     }
 }
